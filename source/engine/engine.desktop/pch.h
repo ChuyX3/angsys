f1f147200464cf38e13d121e26904e9e77cbb434
ChuyX3 <chuyangel.rm@gmail.com>
2017-05-30 00:26:43 -0700

    * adding engine project

---
+++ source/engine/engine.desktop/pch.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <angsys.h>
+#include <ang/collections/vector.h>
+#include <ang/collections/map.h>
+#include <ang/core/time.h>
+
+#include <ang/platform/platform.h>
+#include <ang/platform/angwin/angwin.h>
+
+#include <ang/graphics/angraph.h>
+
+#include <ang/maths/maths.h>
+#include <ang/maths/vectors.h>
+
+#include <ang/com_wrapper.h>
+
+#include <d3d11.h>
+#include <d3d11_2.h>
+#include <d3dcompiler.h>
